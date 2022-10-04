a4a4228f61433b8925a535eddbb021cdd391fd67
guanglun <272073020@qq.com>
2022-10-03 19:14:21 -0700

    update

--- build.sh
+++ build.sh
@@ -8,5 +8,6 @@
 # EXPERT=yes \
 # KERNEL_CONFIGURE=no
 
-./compile.sh  BOARD=maixsense BRANCH=edge RELEASE=focal CLEAN_LEVEL= BUILD_KSRC=no BUILD_MINIMAL=yes BUILD_DESKTOP=no KERNEL_ONLY=yes KERNEL_CONFIGURE=no COMPRESS_OUTPUTIMAGE=sha,gpg,img
+#./compile.sh  BOARD=maixsense BRANCH=edge RELEASE=focal CLEAN_LEVEL=make,image,debs,cache BUILD_KSRC=no BUILD_MINIMAL=yes BUILD_DESKTOP=no KERNEL_ONLY=yes KERNEL_CONFIGURE=no COMPRESS_OUTPUTIMAGE=sha,gpg,img
+./compile.sh  BOARD=maixsense BRANCH=edge RELEASE=focal BUILD_MINIMAL=yes BUILD_DESKTOP=no KERNEL_ONLY=no KERNEL_CONFIGURE=no COMPRESS_OUTPUTIMAGE=sha,gpg,img
 # sudo dd bs=1M if=output/images/Armbian_21.08.0-trunk_Maixsense_focal_edge_5.14.0_minimal.img of=/dev/sdb
